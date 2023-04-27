47f9bfc7bb005a951fad2a1ab84bd27759cbd7d0
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2023-04-27 17:37:30 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,16 +1,15 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+
 # Load the GPT4ALL model.
-
-tokenizer = AutoTokenizer.from_pretrained("nomic-ai/gpt4all-j")
-
-model = AutoModelForCausalLM.from_pretrained("nomic-ai/gpt4all-j")
+tokenizer = transformers.AutoTokenizer.from_pretrained("nomic-ai/gpt4all-j")
+model = transformers.AutoModelForCausalLM.from_pretrained("nomic-ai/gpt4all-j")
 
 # Load the training data.
-train_dataset = transformers.datasets.TextDataset(
+train_dataset = transformers.TextDataset(
     "train.txt",
     tokenizer=tokenizer,
     max_length=512,
-    shuffle=True,
+    block_size=128,
 )
 
 # Define the training parameters.
@@ -18,13 +17,33 @@ num_epochs = 10
 learning_rate = 3e-5
 batch_size = 32
 
+# Initialize the optimizer and the scheduler.
+optimizer = transformers.AdamW(model.parameters(), lr=learning_rate)
+scheduler = transformers.get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=len(train_dataset)*num_epochs)
+
 # Train the model.
-model.fit(
-    train_dataset,
-    epochs=num_epochs,
+training_args = transformers.TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=num_epochs,
+    per_device_train_batch_size=batch_size,
+    save_steps=5000,
     learning_rate=learning_rate,
-    batch_size=batch_size,
+    warmup_steps=0,
+    weight_decay=0.01,
+    logging_dir='./logs',
+    logging_steps=500,
+    dataloader_num_workers=4,
+    seed=42,
 )
+trainer = transformers.Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False),
+    optimizer=optimizer,
+    scheduler=scheduler,
+)
+trainer.train()
 
 # Save the fine-tuned model.
 model.save_pretrained("finetuned_model")
